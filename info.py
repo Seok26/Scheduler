7b61f8c5c576d66024fbb0bc40349bbaf3c844c7
shino <shinoatsuki@gmail.com>
2023-01-24 18:08:25 +0900

    Info.py UI Update 0.3 ver  - Part Information & Cost Insert Box Create By Seok 23.1.24

--- info.py
+++ info.py
@@ -6,9 +6,139 @@ def start_info():
     tk.geometry("1280x800+100+100") #Window 크기 설정
     tk.resizable(True, True) #Window 크기 조절 여부(상하, 좌우)
 
-    title_label = Label(tk, text = '아르바이트 정보 입력', relief = 'raised', bd = 2, width = 30, height = 5, font = 20)
+    frame1 = Frame(tk, relief = 'solid', bd = 2)
+    frame1.pack(side = 'left', fill = 'both', expand=True)
+
+    frame2 = Frame(tk, relief = "solid", bd = 2)
+    frame2.pack(side="right", fill= "both", expand = True)
+
+
+    title_label = Label(frame1, text = '아르바이트 정보 입력', relief = 'raised', bd = 2, width = 30, height = 5, font = 20)
     title_label.place(x = 0, y = 0)
 
+    name_label = Label(frame1, text = '파트 정보 입력', relief = 'raised', bd = 2, width = 20, height = 4, font = 20)
+    name_label.place(x = 220, y = 100)
+
+    weekday_label = Label(frame1, text = '평일', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    weekday_label.place(x = 150, y = 275)
+
+    weekend_label = Label(frame1, text = '주말', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    weekend_label.place(x = 380, y = 275)
+
+    costset_button = Button(frame1, text = '급여 일괄 적용', overrelief= "solid", bg = 'white', width = 12, height = 2, font = 12)
+    costset_button.place(x = 15, y = 285)
+
+    announce_label = Label(frame1, text = '사용하지 않는 파트 칸은 비워두세요.', relief = 'groove', bd = 2, width = 40, height = 3, font = 15)
+    announce_label.place(x = 140, y = 200)
+
+    #파트 1 입력 칸
+    #평일 파트 1
+    part1_label = Label(frame1, text = '파트 1', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    part1_label.place(x = 20, y = 350)
+
+    part1_entry_start = Entry(frame1, width = 16, justify = 'center')
+    part1_entry_start.place(x= 150, y = 350)
+    part1_entry_start.insert(0, "시작 시간") #시작시간 / 종료시간 안내는 파트 1 버튼에서만 수행
+
+    part1_entry_end = Entry(frame1, width= 16, justify= 'center')
+    part1_entry_end.place(x = 150, y = 385)
+    part1_entry_end.insert(0, "종료 시간")
+
+    #급여 입력 칸
+    part1_text_cost = Text(frame1, width = 10, height = 2, font = 12)
+    part1_text_cost.place(x = 278, y = 360)
+    part1_text_cost.insert(1.0, "급여 입력")
+
+    #주말 파트 1
+    part1_entry_start_week = Entry(frame1, width = 16, justify= 'center')
+    part1_entry_start_week.place(x = 378, y = 350)
+
+    part1_entry_end_week = Entry(frame1, width = 16, justify=  'center')
+    part1_entry_end_week.place(x = 378, y = 385)
+
+    #급여 입력 칸
+    part1_text_cost_week = Text(frame1, width = 10, height = 2, font = 12)
+    part1_text_cost_week.place(x = 506, y = 360)
+
+    #파트 2 입력 칸
+    #평일 파트 2
+    part2_label = Label(frame1, text = '파트 2', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    part2_label.place(x = 20, y = 450)
+
+    part2_entry_start = Entry(frame1, width = 16, justify= 'center')
+    part2_entry_start.place(x = 150, y = 450)
+
+    part2_entry_end = Entry(frame1, width = 16, justify= 'center')
+    part2_entry_end.place(x = 150, y = 485)
+
+    #급여 입력 칸
+    part2_text_cost = Text(frame1, width = 10, height = 2, font = 12)
+    part2_text_cost.place(x = 278, y = 460)
+
+    #주말 파트 2
+    part2_entry_start_week = Entry(frame1, width = 16, justify= 'center')
+    part2_entry_start_week.place(x = 378, y = 450)
+
+    part2_entry_end_week = Entry(frame1, width = 16, justify= 'center')
+    part2_entry_end_week.place(x = 378, y = 485)
+
+    #급여 입력 칸
+    part2_text_cost_week = Text(frame1, width = 10, height = 2, font = 12)
+    part2_text_cost_week.place(x = 506, y = 460)
+
+    #파트 3 입력 칸
+    #평일 파트 3
+    part3_label = Label(frame1, text = '파트 3', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    part3_label.place(x = 20, y = 550)
+
+    part3_entry_start = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_start.place(x = 150, y = 550)
+
+    part3_entry_end = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_end.place(x = 150, y = 585)
+
+    #급여 입력 칸
+    part3_text_cost = Text(frame1, width = 10, height = 2, font = 12)
+    part3_text_cost.place(x = 278, y = 560)
+
+    #주말 파트 3
+    part3_entry_start_week = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_start_week.place(x = 378, y = 550)
+
+    part3_entry_end_week = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_end_week.place(x = 378, y = 585)
+
+    #급여 입력 칸
+    part3_text_cost_week = Text(frame1, width = 10, height = 2, font = 12)
+    part3_text_cost_week.place(x = 506, y = 560)
+
+    #파트 4 입력 칸
+    #평일 파트 4
+    part3_label = Label(frame1, text = '파트 4', relief = 'raised', bd = 2, width = 12, height = 3, font = 15)
+    part3_label.place(x = 20, y = 650)
+
+    part3_entry_start = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_start.place(x = 150, y = 650)
+
+    part3_entry_end = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_end.place(x = 150, y = 685)
+
+    #급여 입력 칸
+    part4_text_cost = Text(frame1, width = 10, height = 2, font = 12)
+    part4_text_cost.place(x = 278, y = 660)
+
+    #주말 파트 4
+    part3_entry_start_week = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_start_week.place(x = 378, y = 650)
+
+    part3_entry_end_week = Entry(frame1, width = 16, justify= 'center')
+    part3_entry_end_week.place(x = 378, y = 685)
+
+    #급여 입력 칸
+    part4_text_cost_week = Text(frame1, width = 10, height = 2, font = 12)
+    part4_text_cost_week.place(x = 506, y = 660)
+
+    """
     #요일 Label
     mon_label = Label(tk, text = '월', relief = 'raised', bd = 2, width = 12, height = 3, font = 5)
     mon_label.place(x = 300, y = 80)
@@ -30,10 +160,8 @@ def start_info():
 
     sun_label = Label(tk, text = '일', relief = 'raised', bd = 2, width = 12, height = 3, font = 5)
     sun_label.place(x = 1020, y = 80)
+    """
 
 
-    #파트 정보 입력
-    part1_entry = Entry(tk, width = 20, bd = 2, font = 10)
-    part1_entry.place(x = 100, y = 150)
 
     tk.mainloop()
