4d5afd36648bbd06978afe4db25b69ca72107f02
shino <shinoatsuki@gmail.com>
2023-01-24 13:50:55 +0900

    Main Page UI Setting 0.1ver

--- Main.py
+++ Main.py
@@ -1,7 +1,34 @@
 from tkinter import *
 tk = Tk()
-tk.title("길이 변환기")
+tk.title("Part Timer Scheduler") #제목
+tk.geometry("640x400+100+100") #Window 크기 설정
+tk.resizable(True, True) #Window 크기 조절 여부(상하, 좌우)
 
+title_label = Label(tk, text = 'Part Timer Scheduler', relief = 'raised', bd = 2, width = 30, height = 5, font = 20)
+title_label.pack(side='top')
+
+#시간표 조회 Button
+search_button = Button(tk, text = '시간표 조회', width = 30, height = 3, padx = 10, pady = 10)
+search_button.place(x = 200, y = 100)
+
+#아르바이트 정보 입력 Button
+input_button = Button(tk, text = '아르바이트 정보 입력', width = 30, height = 3, padx = 10, pady = 10)
+input_button.place(x = 200, y = 200)
+
+#급여 조회 Button
+cost_button = Button(tk, text = '급여 조회', width = 30, height = 3, padx = 10, pady = 10)
+cost_button.place(x = 200, y = 300)
+
+def click_search_button():
+
+
+
+def click_input_button():
+
+
+def click_cost_button():
+
+"""
 def Ft2Cm():
     ft2cm = entry1.get()
     entry2.delete(0, "end")
@@ -29,7 +56,6 @@ entry2.grid(row = 1, column = 1)
 btn1 = Button(tk, text = 'ft -> cm', bg = 'black', fg = 'white', command = Ft2Cm).grid(row = 2, column = 0)
 btn2 = Button(tk, text = 'cm -> ft', bg = 'black', fg = 'white', command = Cm2Ft).grid(row = 2, column = 1)
 btn3 = Button(tk, text = 'Reset', bg = 'red', command = reset).grid(row = 3)
-
+"""
 tk.mainloop()
-
 
