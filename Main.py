5b16f86aba5f0e4ab8d4fd20ad761a1dcf3b8fca
shino <shinoatsuki@gmail.com>
2023-01-28 18:51:45 +0900

    주석 추가

--- Main.py
+++ Main.py
@@ -24,15 +24,15 @@ tk.resizable(True, True) #Window 크기 조절 여부(상하, 좌우)
 title_label = Label(tk, text = 'Part Timer Scheduler', relief = 'raised', bd = 2, width = 30, height = 5, font = 20)
 title_label.pack(side='top')
 
-    #시간표 조회 Button
+#시간표 조회 Button
 search_button = Button(tk, text = '시간표 조회', command = enter_search, width = 30, height = 3, padx = 10, pady = 10)
 search_button.place(x = 200, y = 100)
 
-    #아르바이트 정보 입력 Button
+#아르바이트 정보 입력 Button
 input_button = Button(tk, text = '아르바이트 정보 입력', command = enter_info, width = 30, height = 3, padx = 10, pady = 10)
 input_button.place(x = 200, y = 200)
 
-    #급여 조회 Button
+#급여 조회 Button
 cost_button = Button(tk, text = '급여 조회', command = enter_cost, width = 30, height = 3, padx = 10, pady = 10)
 cost_button.place(x = 200, y = 300)
 
